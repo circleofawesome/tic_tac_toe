8b6da4525ff7241e3b1667edbc0e23cc5d427fc5
Samiul Anwar <samiul.anwar@gmail.com>
2016-03-10 12:16:24 -0500

    adding instructions and getting the game started

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,13 +1,42 @@
 class Grid
-	def empty_grid(m)
-		puts" #{m} | #{m} | #{m} "
+	def grid(a,b,c,d,e,f,g,h,i)
+		puts" #{a} | #{b} | #{c} "
 		puts"___|___|___"
-		puts" #{m} | #{m} | #{m} "
+		puts" #{d} | #{e} | #{f} "
 		puts"___|___|___"
-		puts" #{m} | #{m} | #{m} "
+		puts" #{g} | #{h} | #{i} "
 		puts"   |   |   "
+	end
+	def marked_grid
+		
 	end
 end
 
-bo=Grid.new
-bo.empty_grid("X")
+puts " /$$$$$$$$ /$$                 /$$$$$$$$                        /$$$$$$$$                 
+|__  $$__/|__/                |__  $$__/                       |__  $$__/                 
+   | $$    /$$  /$$$$$$$         | $$  /$$$$$$   /$$$$$$$         | $$  /$$$$$$   /$$$$$$ 
+   | $$   | $$ /$$_____/         | $$ |____  $$ /$$_____/         | $$ /$$__  $$ /$$__  $$
+   | $$   | $$| $$               | $$  /$$$$$$$| $$               | $$| $$  \ $$| $$$$$$$$
+   | $$   | $$| $$               | $$ /$$__  $$| $$               | $$| $$  | $$| $$_____/
+   | $$   | $$|  $$$$$$$         | $$|  $$$$$$$|  $$$$$$$         | $$|  $$$$$$/|  $$$$$$$
+   |__/   |__/ \_______/         |__/ \_______/ \_______/         |__/ \______/  \_______/
+                                                                                          
+                                                                                          
+                                                                                          "
+
+
+puts "Let's play Tic Tac Toe!"
+print "Player 1 choose X or O: "
+player1=gets.chomp
+while player1!="X"&&player1!="O"&&player1!="x"&&player1!="o"
+	print"Please select X or O: "
+	player1=gets.chomp
+end
+player2=nil
+if player1=="X"||player1=="x"
+	puts "Excellent, player 2 you are O!"
+	player2="O"
+elsif player1=="O"||player1=="o"
+	puts "Excellent, player 2 you are X!"
+	player2="X"
+end
